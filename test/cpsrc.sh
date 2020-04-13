9399f99b726db54dfaf9800e98bdfe67ba5b9c6c
feifei14119 <Fei.Wang@amd.com>
2020-04-13 09:27:43 +0800

    updata test

---
+++ test/cpsrc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh 
+
+rm Cijk_Alik_Bljk_HBH_MT128x64x32_SE_K1.o
+rm Cijk_Alik_Bljk_HBH_MT128x64x32_SE_K1.co
+rm Cijk_Alik_Bljk_HBH_MT128x64x32_SE_K1.inc
+
+cp ~/sp3/GEMM_FP16_128hx64wx32u_2wave.inc Cijk_Alik_Bljk_HBH_MT128x64x32_SE_K1.inc
